3655bcd44c5e5aa4a6e80f9eb573abe0b2a82874
BrightProgrammer7 <bright.procoder@gmail.com>
2024-03-29 03:19:58 +0000

    voixoff.ma

---
+++ .history/config/firebase_20240327195814.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyByCvwI1zJgENrjrgjIKIMoVkX4XAp0CeU",
+    authDomain: "voix-off-ai.firebaseapp.com",
+    projectId: "voix-off-ai",
+    storageBucket: "voix-off-ai.appspot.com",
+    messagingSenderId: "1098652714736",
+    appId: "1:1098652714736:web:03c8d97d28caacefa4a435"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
---
+++ .history/config/script_20240327224716.js
@@ -0,0 +1,13 @@
+function googleLogin() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+
+    .then((result) => {
+      const user = result.user;
+    });
+
+    console.log(user);
+}
